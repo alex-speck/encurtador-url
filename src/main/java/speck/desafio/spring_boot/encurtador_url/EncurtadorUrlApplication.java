764f5c56d5533628b72f7d6e9b08cf4dbb345713
alex-speck <alexbaranoski08@gmail.com>
2025-12-26 20:08:26 -0300

    primeiro commit

---
+++ src/main/java/speck/desafio/spring_boot/encurtador_url/EncurtadorUrlApplication.java
@@ -0,0 +1,13 @@
+package speck.desafio.spring_boot.encurtador_url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorUrlApplication.class, args);
+	}
+
+}
